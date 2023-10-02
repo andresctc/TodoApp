e46355193aa9f956a7ecc6cacedbe815255d7d2a
Andres Camilo Torres C <andresctc@gmail.com>
2023-10-02 13:25:09 -0500

    Spring Initializr

---
+++ src/test/java/com/hardsoft/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardsoft.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
